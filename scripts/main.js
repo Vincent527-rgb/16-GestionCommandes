0f99803223f24d11917b7df6c38820ca34c0dd83
Vincent527-rgb <vincentcss16@gmail.com>
2025-05-16 14:22:22 +0200

    End

--- scripts/main.js
+++ scripts/main.js
@@ -25,10 +25,25 @@ function addOrder(name, price, quantity) {
         alert("Veuillez remplir les champs")
     } else {
         tableauCommandes.splice(tableauCommandes.length, 0, new Order(name, price, quantity));
+        globalPrice()
     }
 }
 function displayOrder(name, price, quantity) {
-    listCommandes.innerHTML = `${quantity}x ${name} - Total: ${price}`
+    // listCommandes.innerHTML += `<div>${quantity}x ${name} - Total: ${price.toFixed(2)}€</div>`;
+    
+    const div = document.createElement("div");
+    listCommandes.appendChild(div);
+
+    const par = document.createElement("div")
+    par.textContent = `${quantity}x ${name} - Total: ${price.toFixed(2)}€`;
+    div.appendChild(par);
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.className = "delete-btn";
+    deleteBtn.title = "Supprimer l'élément";
+    deleteBtn.dataset = ""
+    deleteBtn.textContent = "❌";
+    div.appendChild(deleteBtn);
 }
 function resetInput() {
     inputName.value = "";
@@ -37,8 +52,8 @@ function resetInput() {
     inputName.focus();
 }
 function globalPrice() {
-    const sumTotalPrice = tableauCommandes.reduce((sum, currentValue) => sum + currentValue.price, 0,);
-        totalCommandes.innerHTML = `Total global: ${sumTotalPrice}€`
+    const sumTotalPrice = tableauCommandes.reduce((sum, currentValue) => sum + currentValue.totalPrice(), 0);
+    totalCommandes.innerHTML = `Total global: ${sumTotalPrice.toFixed(2)}€`
 }
 
 // ==== Evénements ====
@@ -49,6 +64,18 @@ addBtn.addEventListener("click", function (e) {
     const quantity = parseFloat(inputQuantity.value);
 
     addOrder(name, price, quantity);
-    displayOrder(name, price, quantity);
+    displayOrder(name, price*quantity, quantity);
     resetInput();
+})
+
+listCommandes.addEventListener("click", function (event) {
+    if(event.target.classList.contains("delete-btn")) {
+        const item = event.target.parentElement;
+        const indexItem = Array.from(event.target.parentElement.children).indexOf(item);
+        item.remove();
+        tableauCommandes.splice(indexItem, 1);
+        console.log(item);
+        console.log(indexItem);
+        
+    }
 })
